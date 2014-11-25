0cc69305b3a5ec11a41520a116a3f57486e28483
mightykyle <kmbraul@ilstu.edu>
2014-11-25 10:34:30 -0600

    this has new shit!

---
+++ src/java/controller/ImageView.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author itmacuser
+ */
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImageView {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 1; i++) {
+            images.add("kyle" + i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
