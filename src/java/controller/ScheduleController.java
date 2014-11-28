a2009a6400c4c69d888c416faf2d8e5368cfb2c3
mightykyle <kmbraul@ilstu.edu>
2014-11-28 12:09:48 -0600

    added some scheduleing stuff

--- src/java/controller/ScheduleController.java
+++ src/java/controller/ScheduleController.java
@@ -14,10 +14,17 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Properties;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import model.ScheduleBean;
 import org.primefaces.context.RequestContext;
 import org.primefaces.event.SelectEvent;
@@ -65,7 +72,7 @@ public class ScheduleController implements Serializable {
         int rowCount = aScheduleDAO.createSchedule(theModel); // Doing anything with the object after this?
 
         if (rowCount == 1) {
-
+            sendEmail();
             return "confirmation.xhtml"; // navigate to "ECHO.xhtml"
         } else if (rowCount == 2) {
             return "nolonger.xhtml";
@@ -85,6 +92,53 @@ public class ScheduleController implements Serializable {
 
         requestContext.update("form:display");
         requestContext.execute("PF('dlg').show()");
+    }
+    public void sendEmail(){
+        // Recipient's email ID needs to be mentioned.
+        String to = "kmbraul@ilstu.edu";
+
+        // Sender's email ID needs to be mentioned
+        String from = "Administrator@linkedU.com";
+
+        // Assuming you are sending email from this host
+        String host = "smtp.ilstu.edu";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.user", "kmbraul@ilstu.edu"); // if needed
+        properties.setProperty("mail.password", "ppiceee8"); // if needed
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("Appointment !");
+
+            // Send the actual HTML message, as big as you like
+            message.setContent("<h1>Your Appointment " + theModel.getGroupname() + " will be on " + theModel.getDate() + " at " + theModel.getTimeslot() + ".<h1>",
+                    "text/html");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
     }
 
     
